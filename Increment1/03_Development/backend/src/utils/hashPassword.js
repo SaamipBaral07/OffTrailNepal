03d0bf902fe1276b0cf2e4bdd63f0cb60ff584d5
SaamipBaral07 <baralsamip4@gmail.com>
2026-04-20 17:52:14 +0545

    chore: keep only Increment3 folder on Increment3

--- Increment1/03_Development/backend/src/utils/hashPassword.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-const run = async () => {
-  const password = "admin123"; // choose your admin password
-  const hash = await bcrypt.hash(password, 10);
-  console.log("HASHED PASSWORD:", hash);
-};
-
-run();
